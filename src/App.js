0778a19256c34f8274e766c8f9e0b4688e36bf60
minechoo <choominhae@gmail.com>
2023-08-02 11:06:18 +0900

    useTransition을 이용해서 우선 순위가 낮은 연산을 늦게 처리하게 해서 페이지 로딩성능 개선

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import { useState } from 'react';
-import { flushSync } from 'react-dom';
+import { useState, useTransition } from 'react';
 
 function App() {
-	console.log('render!');
 	const [Count, setCount] = useState(0);
-	const [Count2, setCount2] = useState(0);
-	const [Count3, setCount3] = useState(0);
+	const [Items, setItems] = useState([]);
+	const [isPadding, startTansition] = useTransition();
 
-	const returnPromise = () => {
-		return new Promise((res) => setTimeout(res, 500));
-	};
+	console.log('isPadding', isPadding);
 
+	//아래함수에서는 정 중요하고 무거운 연산때문에 급한 연산까지 덩달아 늦게 화면에 랜더링
 	const handleClick = () => {
-		returnPromise().then(() => {
-			flushSync(() => setCount(Count + 1));
+		//urgent op ( 급하게 처리되어야 할 중요한 연산)
+		setCount(Count + 1);
 
-			setCount2(Count2 + 2);
-			setCount3(Count3 + 3);
+		startTansition(() => {
+			//not urgent op ( 우선순위가 떨어자는 덜 중요한 연산)
+			const arr = Array(10000)
+				.fill(1)
+				.map((_, idx) => Count + idx);
+			setItems(arr);
 		});
 	};
 
+	//아래 버튼 클릭시마다 만개의 배열 리스트가 출력되기 전까지는 버튼의 숫자값이 늦게 카운트 되는 것을 확인
 	return (
 		<div className='App'>
-			<h1>Hello</h1>
-
-			<button onClick={handleClick}>button</button>
-			<h1 style={{ color: '#000' }}>
-				{Count} - {Count2} - {Count3}
-			</h1>
+			<button onClick={handleClick} disabled={isPadding}>
+				{Count}
+			</button>
+			<ul>
+				{Items.map((num) => {
+					return <li key={num}>{num}</li>;
+				})}
+			</ul>
 		</div>
 	);
 }
@@ -35,8 +39,8 @@ function App() {
 export default App;
 
 /*
-	Automatic Batching
-	: 핸들러함수 안쪽에서 복수개의 state값이 변경될때 해당 변경사항을 묶어서(batching)해서 한번만 리랜더링
-	: 기존 17버전에서는 promise를 반환하는 함수 안에서는 auto batching동작 불가 (18버전에서 개선됨)
-  : 리액트 18에서 동일 핸들러함수 안쪽에 있더라도 flushSync를 이용해 특정 State만 auto batching에서 제외 처리 가능
+  useTransition
+	연산의 우선순위를 주어서 좀 늦게 랜더링 해도 될 것을 지정
+  기존에는 한 번 랜더링 연산이 시작되면 중간에 멈출 수 없었음
+  특정 핸들러함수에 의해서 화면을 재 연산해야 되는 경우 중간에 무거운 로직이 실행되는 연산이 있으면 나모지 연산도 같이 지연이 일어남
 */
